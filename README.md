9c4f26b58dbc1b4d28db3e8d4a5de5cf37c49830
Gr%1m <1299091518@qq.com>
2024-08-19 09:49:22 +0800

    v0.6.3

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+port info data from [lcvvvv/gonmap](https://github.com/lcvvvv/gonmap.git)
+
+
--- gscan.go
+++ gscan.go
@@ -5,11 +5,40 @@ import (
 	"net"
 	// "sync"
 	"flag"
+	"gscan/config"
+	"os"
 	"sort"
+	"strings"
 	"time"
 )
 
 var version = "v0.6.1"
+
+func portInfo() {
+	// This function is Copy from another file, still in the Development Stage
+
+	var args string
+	var ports []int
+	var dfps = []int{23, 25, 53, 88, 139, 389, 443, 445, 3389, 5432}
+	// default show some port info
+
+	if len(os.Args) > 1 {
+		args = strings.Join(os.Args[1:], "")
+		ports = config.PortListProc(args)
+	} else {
+		ports = dfps
+	}
+	infos := config.NmapServices
+
+	fmt.Println("")
+	for _, port := range ports {
+		fmt.Printf("%d: %s\n", port, infos[port])
+	}
+
+	//for port, info := range config.PortInfoList(ports) {
+	//	fmt.Printf("%d: %s\n", port, info)
+	//}
+}
 
 func portScan(host string, timeout int, thread int) []int {
 	// The default recommended Thread is 700
@@ -73,18 +102,38 @@ func main() {
 
 	var openports []int
 
+	timeout := flag.Int("to", 60, "Config Output nmapCommand max port number")
+	threads := flag.Int("th", 650, "Config Output nmapCommand max port number")
+	max_portnum := flag.Int("n", 20, "Config Output nmapCommand max port number")
 	target := flag.String("ip", "", "The Target Host IPaddress for Scan")
-	thread := flag.Int("th", 650, "Thread")
-	timeout := flag.Int("to", 60, "Timeout unit: ms")
+	pinfo := flag.Bool("pi", false, "Still in the Development Stage")
 	flag.Parse()
+
+	if *pinfo {
+		portInfo()
+		return
+	}
+
 	if *target == "" {
 		flag.Usage()
 		return
 	}
-	openports = portScan(*target, *timeout, *thread)
+	openports = portScan(*target, *timeout, *threads)
+	portnum := len(openports)
 
 	for _, port := range openports {
 		fmt.Printf("%d is open\n", port)
 	}
+	// fmt.Printf("[*] 扫描结束,耗时: %s\n\n", time.Since(start))
 	fmt.Printf("[*] Gscan Finish, scanned in: %s\n", time.Since(start))
+
+	if portnum < *max_portnum {
+		fmt.Printf("You can use: \n\t nmap %s -p", *target)
+		for _, port := range openports {
+			fmt.Printf("%d,", port)
+		}
+		fmt.Println("\b \n\nTo Check services running on these ports")
+	} else {
+		fmt.Printf("Open ports is too many(%d), if you want use nmap You can add the -n to config maxOutputPortNum", portnum)
+	}
 }
